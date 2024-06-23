142330a1e7259cc0b2bb08b8fec68b01a55a038a
meherm <meherpurvi2815@gmail.com>
2024-06-23 18:38:56 +0100

    basic auth added to all endpoints

--- springdemo/src/main/java/com/example/springdemo/config/SecurityConfig.java
+++ springdemo/src/main/java/com/example/springdemo/config/SecurityConfig.java
@@ -1,14 +1,19 @@
 package com.example.springdemo.config;
+/*
 
+import io.swagger.models.HttpMethod;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity request) throws Exception{
-        request.csrf().disable();
+        request.csrf().disable().authorizeRequests().anyRequest().permitAll();
+//        request.authorizeRequests().anyRequest().permitAll();
         return request.build();
     }
 }
+*/
